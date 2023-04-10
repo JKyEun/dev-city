b00d8bd8edc8fb2ba49132175e43d9019e02c2d0
minjeong <kmjeongk@gmail.com>
2023-04-10 09:30:25 +0900

    Feat:mainpage생성, recentStudy불러오기

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import { Route, Routes } from "react-router-dom";
-import "./App.css";
-import Header from "./components/Header";
-import MyCityPage from "./pages/MyCityPage";
-import RecruitBoardPage from "./pages/RecruitBoardPage";
-import CreateStudyPage from "./pages/CreateStudyPage";
-
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Header from './components/Header';
+import MyCityPage from './pages/MyCityPage';
+import RecruitBoardPage from './pages/RecruitBoardPage';
+import CreateStudyPage from './pages/CreateStudyPage';
+import MainPage from './pages/MainPage';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Routes>
+        <Route path="/" element={<MainPage />} />
         <Route path="/mycity" element={<MyCityPage />} />
-
         <Route path="/study" element={<RecruitBoardPage />} />
         <Route path="/study/create" element={<CreateStudyPage />} />
-
       </Routes>
     </div>
   );
---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../style/main.scss';
+import MainStudyView from '../components/MainViewStudy/MainStudyView';
+export default function MainPage() {
+  return (
+    <>
+      {/* 메인 */}
+      <div className="mainTop">
+        <div className="minMax">
+          <div className="main flexBox">
+            <div className="mainText">
+              <div className="text">
+                개발공부는
+                <br /> 데브시티에서
+              </div>
+              <div className="mycityLink">
+                <Link to={'/mycity'}>나의 도시 바로가기</Link>
+              </div>
+            </div>
+            <img
+              className="mycityImg"
+              src="../images/mycity.svg"
+              alt="도시이미지"
+            />
+          </div>
+        </div>
+      </div>
+      {/* 모집 중인 스터디 */}
+      <div className="studyRecruit">
+        <div className="minMax">
+          <div className="flexBox">
+            <div className="subText">모집 중인 스터디</div>
+            <p>
+              <Link to={'/study'}>더보기+</Link>
+            </p>
+          </div>
+          <div>
+            <div className="flexBox">
+              <MainStudyView />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* 스터디 생성 프로세스 */}
+      <div className="process">
+        <div className="minMax">
+          <div className="subText">스터디 생성 프로세스</div>
+          <div className="flexBox">
+            <div className="step">
+              <img src="/images/step1.png" alt="" />
+              <div className="title">STEP.1 스터디 생성하기</div>
+              <p className="content">
+                오른쪽 상단에 '스터디 생성하기' 버튼을 이용해서 생성해주세요
+              </p>
+            </div>
+            <div className="step">
+              <img src="/images/step2.png" alt="" />
+              <div className="title">STEP.2 스터디 상세 설정하기</div>
+              <p className="content">
+                스터디 이름, 주제, 목적, 인원 수 등 스터디에 필요한 정보를
+                입력하여 스터디를 생성합니다.
+              </p>
+            </div>
+            <div className="step">
+              <img src="/images/step3.png" alt="" />
+              <div className="title">STEP.3 스터디 홍보하기</div>
+              <p className="content">
+                스터디 링크를 생성하고, 다른 사람들에게 공유합니다.sns, 커뮤니티
+                등을 이용하여 홍보합니다.
+              </p>
+            </div>
+            <div className="step">
+              <img src="/images/mycity.png" alt="" />
+              <div className="title">STEP.4 사람들과 공부하기</div>
+              <p className="content">
+                스터디 규칙을 준수하며, 서로 도움을 주고 받으면서 스터디를
+                진행합니다.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* 추천친구 */}
+    </>
+  );
+}
