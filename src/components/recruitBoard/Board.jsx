7cb556fb83b4c70d2fb92fdacba19a915fadee0f
KingJiwon <wldnjs0401@naver.com>
2023-04-10 14:30:24 +0900

    Feat: 참가버튼,좋아요 아이콘 추가(기능x)

--- src/components/recruitBoard/Board.jsx
+++ src/components/recruitBoard/Board.jsx
@@ -1,4 +1,5 @@
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import '../../style/recruitBoard/Board.scss';
 
 export default function Board() {
@@ -9,30 +10,43 @@ export default function Board() {
     studies.map((el, idx) => {
       return (
         <div className="study" key={`study_${idx}`}>
-          <div className="study_info-text">
-            <div className="study_info-text-small">
-              <div className="member_num">
-                {el.memberNum.currentNum} / {el.memberNum.maxNum}
+          <div className="study_info">
+            <div className="study_info-text">
+              <div className="study_info-text-small">
+                <div className="member_num">
+                  {el.memberNum.currentNum}/{el.memberNum.maxNum}
+                </div>
+                <div className="create_date">
+                  {new Date(el.createDate).toISOString().substring(0, 10)}
+                </div>
               </div>
-              <div className="create_date">
-                {new Date(el.createDate).toISOString().substring(0, 10)}
-              </div>
+              <div className="study_intro">{el.studyIntro}</div>
             </div>
-            <div className="study_intro">{el.studyIntro}</div>
+            <div className="study_info-skill">
+              {el.skills.map((skill) => {
+                return (
+                  <img
+                    src={`/images/skill_icon/${skill}.svg`}
+                    alt={`${skill}이미지`}
+                  />
+                );
+              })}
+            </div>
           </div>
-          <div className="study_info-skill">
-            {el.skills.map((skill) => {
-              return (
-                <img
-                  src={`/images/skill_icon/${skill}.svg`}
-                  alt={`${skill}이미지`}
-                />
-              );
-            })}
+          <div className="study_button">
+            <ul>
+              <li className="study_button-participate">
+                <Link to={'./study/detail'}>참가하기</Link>
+              </li>
+              <li className="interst">
+                <span>
+                  <img src="./images/icon_heartOff.svg" alt="heart" />
+                </span>
+              </li>
+            </ul>
           </div>
         </div>
       );
     });
-
   return <div className="study_board">{studiesRender}</div>;
 }
