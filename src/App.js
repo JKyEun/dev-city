00520fb09edb84de548509c6785fc1845a12c4f5
HeeHeeHee <praaa0220@gmail.com>
2023-04-10 09:15:43 +0900

    Feat: study 생성

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import { Route, Routes } from "react-router-dom";
-import "./App.css";
-import Header from "./components/Header";
-import MyCityPage from "./pages/MyCityPage";
-import RecruitBoardPage from "./pages/RecruitBoardPage";
-import CreateStudyPage from "./pages/CreateStudyPage";
-
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Header from './components/Header';
+import MyCityPage from './pages/MyCityPage';
+import RecruitBoardPage from './pages/RecruitBoardPage';
+import CreateStudyPage from './pages/CreateStudyPage';
+import StudyDetailPage from './pages/StudyDetailPage';
 
 function App() {
   return (
@@ -14,8 +14,8 @@ function App() {
         <Route path="/mycity" element={<MyCityPage />} />
 
         <Route path="/study" element={<RecruitBoardPage />} />
+        <Route path="/study/detail" element={<StudyDetailPage />} />
         <Route path="/study/create" element={<CreateStudyPage />} />
-
       </Routes>
     </div>
   );
