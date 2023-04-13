057a5bdd2af7b827ca30865575b4e22082129e03
KingJiwon <wldnjs0401@naver.com>
2023-04-13 10:07:09 +0900

    Feat: study board에 전체 스터디 개수  출력 기능

--- src/components/recruitBoard/SubHeader.jsx
+++ src/components/recruitBoard/SubHeader.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import '../../style/recruitBoard/SubHeader.scss';
 export default function SubHeader() {
+  const studies = useSelector((state) => state.study.studies);
+  const totalStudyNumber = () => {
+    if (studies !== undefined) {
+      return studies.length;
+    }
+  };
   return (
     <div className="subheader">
       <div className="subheader_text">
         <p>스터디</p>
-        <p>총 ~~개 | 다양한 스터디를 확인 해보세요</p>
+        <p>
+          <span>총 {totalStudyNumber()}개</span> | 다양한 스터디를 확인 해보세요
+        </p>
       </div>
       <div className="subheader_createbutton">
-        <Link to={''}>스터디 생성하기</Link>
+        <Link to={'/study/create'}>스터디 생성하기</Link>
       </div>
     </div>
   );
