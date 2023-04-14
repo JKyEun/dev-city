a60ba80b5ac31f5372238386e390707b0704bde6
JKyEun <j56237@naver.com>
2023-04-15 00:14:29 +0900

    Feat: 게시판 게시글 추가, 삭제 기능

--- src/components/StudyDetail/Post.jsx
+++ src/components/StudyDetail/Post.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import Comment from './Comment';
 import axios from 'axios';
+import { useParams } from 'react-router-dom';
 
-export default function Post({ boardDB, setBoardDB }) {
-  console.log(boardDB);
+export default function Post({ boardDB, el, setBoardDB }) {
+  const { id } = useParams();
   const [writerInfo, setWriterInfo] = useState(null);
   const [isCommentOpen, setIsCommentOpen] = useState(false);
-  const date = new Date(boardDB.date);
+  const date = new Date(el.date);
   const dateFormat =
     date.getFullYear() + '. ' + date.getMonth() + '. ' + date.getDate() + '.';
 
@@ -20,8 +21,27 @@ export default function Post({ boardDB, setBoardDB }) {
     }
   };
 
+  const deletePost = async () => {
+    const deletePost = {
+      id: el.id,
+    };
+
+    try {
+      const res = await axios.post(
+        `http://localhost:4000/board/delete/${id}`,
+        deletePost,
+      );
+      console.log(res.data);
+
+      const newBoardDB = boardDB.filter((el) => el.id !== deletePost.id);
+      setBoardDB(newBoardDB);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   useEffect(() => {
-    getWriterInfo(boardDB.writer);
+    getWriterInfo(el.writer);
   }, []);
 
   return (
@@ -44,26 +64,28 @@ export default function Post({ boardDB, setBoardDB }) {
                 <div className="nickname">{writerInfo.nickName}</div>
                 <div className="date">{dateFormat}</div>
               </div>
-              <div className="content">{boardDB.content}</div>
+              <div className="content">{el.content}</div>
               <div
                 onClick={() => {
                   setIsCommentOpen((cur) => !cur);
                 }}
                 className="getComment"
               >
-                {boardDB.comment.length === 0
+                {el.comment.length === 0
                   ? '댓글 달기'
-                  : `${boardDB.comment.length}개의 댓글`}
+                  : `${el.comment.length}개의 댓글`}
               </div>
               <button className="modifyBtn">수정</button>
-              <button className="deleteBtn">삭제</button>
+              <button onClick={deletePost} className="deleteBtn">
+                삭제
+              </button>
             </div>
           </>
         )}
       </div>
       {isCommentOpen && (
         <div className="commentWrap">
-          {boardDB.comment.map((el) => (
+          {el.comment.map((el) => (
             <Comment key={el.id} commentDB={el} setBoardDB={setBoardDB} />
           ))}
           <form className="commentInput">
--- src/components/StudyDetail/StudyBoard.jsx
+++ src/components/StudyDetail/StudyBoard.jsx
@@ -1,12 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Post from './Post';
 import '../../style/studyBoard.scss';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import { useSelector } from 'react-redux';
 
 export default function StudyBoard() {
   const { id } = useParams();
   const [boardDB, setBoardDB] = useState(null);
+  const userInfo = useSelector((state) => state.user);
+  const boardInput = useRef();
 
   console.log(boardDB);
 
@@ -15,18 +18,49 @@ export default function StudyBoard() {
     setBoardDB(res.data.board);
   };
 
+  const addPost = async (e) => {
+    e.preventDefault();
+
+    const newPost = {
+      id: +new Date(),
+      writer: userInfo.userId,
+      date: new Date().toString(),
+      content: boardInput.current.value,
+      comment: [],
+    };
+    try {
+      const res = await axios.post(
+        `http://localhost:4000/board/add/${id}`,
+        newPost,
+      );
+      console.log(res.data);
+
+      const newBoardDB = [...boardDB, newPost];
+      setBoardDB(newBoardDB);
+
+      boardInput.current.value = '';
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   useEffect(() => {
     getBoard();
   }, []);
+
   return (
     <div className="studyBoardWrap">
       <form className="createPostForm">
-        <input type="text" placeholder="하고 싶은 말을 적어보세요!" />
-        <button>작성하기</button>
+        <input
+          ref={boardInput}
+          type="text"
+          placeholder="하고 싶은 말을 적어보세요!"
+        />
+        <button onClick={(e) => addPost(e)}>작성하기</button>
       </form>
       {boardDB !== null &&
         boardDB.map((el) => (
-          <Post key={el.id} boardDB={el} setBoardDB={setBoardDB} />
+          <Post key={el.id} boardDB={boardDB} el={el} setBoardDB={setBoardDB} />
         ))}
     </div>
   );
