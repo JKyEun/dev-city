a447764a57045d0290e06fec4dc97f4dc0e6b72c
HeeHeeHee <praaa0220@gmail.com>
2023-04-17 13:33:38 +0900

    Feat/ 스터디 상세 페이지 탭 기능

--- src/pages/StudyDetailPage.jsx
+++ src/pages/StudyDetailPage.jsx
@@ -2,13 +2,26 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import StudyDetail from '../components/StudyDetail/StudyDetail';
 import StudyBoard from '../components/StudyDetail/StudyBoard';
+import MemberBox from '../components/StudyDetail/MemberBox';
+import DetailTab from '../components/StudyDetail/DetailTab';
 
 export default function StudyDetailPage() {
   const { id } = useParams();
   return (
-    <>
-      <StudyDetail match={{ params: { id } }} />
-      <StudyBoard />
-    </>
+    // {/* <StudyDetail match={{ params: { id } }} /> */}
+    // <div className="minMax">
+    // <div className="flexBox-between-start">
+    // <DetailTab />
+    // <MemberBox match={{ params: { id } }} />
+    // {/* <StudyBoard /> */}
+    // </div>
+    // </div>
+
+    <div className="minMax">
+      <div className="flexBox-between-start">
+        <DetailTab />
+        <MemberBox match={{ params: { id } }} />
+      </div>
+    </div>
   );
 }
