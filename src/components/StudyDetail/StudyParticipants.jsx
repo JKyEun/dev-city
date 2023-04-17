4582bc364ed83f483ef471c71d6edcedfaf08968
minjeong <kmjeongk@gmail.com>
2023-04-17 09:37:07 +0900

    Design:스터디디테일

--- src/components/StudyDetail/StudyParticipants.jsx
+++ src/components/StudyDetail/StudyParticipants.jsx
@@ -2,6 +2,7 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { init } from '../../store/modules/user';
+import '../../style/studyProfile.scss';
 
 export default function StudyParticipants({ member }) {
   const dispatch = useDispatch();
@@ -20,27 +21,34 @@ export default function StudyParticipants({ member }) {
   useEffect(() => {
     getUserInfo(member.memberId);
   }, []);
-
   return (
-    <div>
-      <div className="profileBox">
+    <div className="profile">
+      <div>
         <img
           className="profilePhoto"
           src={
-            userInfo?.profileImg
+            userInfo?.profileImg?.includes('http')
               ? userInfo?.profileImg
-              : '/images/default-profile.png'
+              : !userInfo?.profileImg
+              ? '/images/default-profile.png'
+              : `http://localhost:4000/uploads/${userInfo?.profileImg?.replace(
+                  '/',
+                  '',
+                )}`
           }
           alt="프로필 사진"
+          width="98"
+          height="98"
         />
+
+        <div className="nickName">{userInfo?.nickName}</div>
+        <div className="userName">{userInfo?.userName}</div>
+        <div>
+          <span className="field">{userInfo?.field}</span>
+          <span className="level">Lv.{userInfo?.level}</span>
+        </div>
+        <button className="btnFollow">팔로우</button>
       </div>
-      <div className="nickName">{userInfo?.nickName}</div>
-      <div className="userName">{userInfo?.userName}</div>
-      <div>
-        <span className="field">{userInfo?.field}</span>
-        <span className="level">Lv.{userInfo?.level}</span>
-      </div>
-      <button className="btnFollow">팔로우</button>
     </div>
   );
 }
