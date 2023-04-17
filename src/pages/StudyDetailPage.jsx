f8070c5c98bcb860977bde939d876e9f59678510
HeeHeeHee <praaa0220@gmail.com>
2023-04-17 19:04:08 +0900

    Feat/ 스터디 수정

--- src/pages/StudyDetailPage.jsx
+++ src/pages/StudyDetailPage.jsx
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import StudyDetail from '../components/StudyDetail/StudyDetail';
 import StudyBoard from '../components/StudyDetail/StudyBoard';
 import MemberBox from '../components/StudyDetail/MemberBox';
 import DetailTab from '../components/StudyDetail/DetailTab';
+import ModifyStudy from '../components/StudyDetail/ModifyStudy';
+import { useSelector } from 'react-redux';
 
 export default function StudyDetailPage() {
   const { id } = useParams();
+  const [isModifyMode, setIsModifyMode] = useState(false);
+  const modifyStatus = useSelector((state) => state.studyDetail.isModify);
   return (
     <div className="minMax">
       <div className="flexBox-between-start">
-        <DetailTab />
-        <MemberBox match={{ params: { id } }} />
+        {modifyStatus ? <ModifyStudy /> : <DetailTab />}
+
+        <MemberBox
+          match={{ params: { id } }}
+          setIsModifyMode={setIsModifyMode}
+        />
       </div>
     </div>
   );
--- src/store/modules/studyDetail.js
+++ src/store/modules/studyDetail.js
@@ -2,12 +2,15 @@
 const initState = {
   study: null,
   loading: false,
+  isModify: false,
 };
 
 // 액션 타입 정의
 const FETCH_STUDY = 'studyDetail/FETCH_STUDY';
 const SET_STUDY = 'studyDetail/SET_STUDY';
 const CLOSE_AND_OPEN_STUDY = 'studyDetail/CLOSE_AND_OPEN_STUDY';
+const MODIFY = 'studyDetail/MODIFY';
+
 // 액션 생성 함수 정의
 export const fetchStudy = (id) => ({
   type: FETCH_STUDY,
@@ -21,6 +24,11 @@ export const setStudy = (study) => ({
 
 export const closeAndOpenStudy = (boolean) => ({
   type: CLOSE_AND_OPEN_STUDY,
+  payload: boolean,
+});
+
+export const modifyStudy = (boolean) => ({
+  type: MODIFY,
   payload: boolean,
 });
 
@@ -42,6 +50,13 @@ const studyDetail = (state = initState, action) => {
       return {
         ...state,
         study: { ...state.study, isClosed: action.payload },
+      };
+
+    case MODIFY:
+      console.log(action.payload);
+      return {
+        ...state,
+        isModify: action.payload,
       };
     default:
       return state;
