39d922bdbe2f25310f817b35d916c58f6391c3a8
KingJiwon <wldnjs0401@naver.com>
2023-04-18 13:31:12 +0900

    Feat: 존재 여부에 따른 Todolist 조건부 렌더링

--- src/components/StudyDetail/StudyStatusTab/TodoList.jsx
+++ src/components/StudyDetail/StudyStatusTab/TodoList.jsx
@@ -1,5 +1,38 @@
 import React from 'react';
 
 export default function TodoList({ data }) {
-  return <div>TodoList</div>;
+  const nickName = data.nickName;
+  const userTodo = data.todoList;
+  return (
+    <>
+      <div className="todo_intro">
+        {nickName === '닉네임을 설정하세요'
+          ? nickName
+          : `${nickName}님의 오늘 할 일`}
+      </div>
+
+      {userTodo.length === 0 ? (
+        <p>오늘 할 일을 설정해 주세요!</p>
+      ) : (
+        userTodo.map((el, idx) => (
+          <div className="todo_main">
+            <div className="checkDiv" key={`userTodo_${idx}`}>
+              <span
+                className={el.isCompleted ? 'checkbox checked' : 'checkbox'}
+              ></span>
+              {el.isCompleted && (
+                <img
+                  src="/images/icon_checked.svg"
+                  alt="체크표시"
+                  className="checkImg"
+                />
+              )}
+            </div>
+            <div className="content"></div>
+            {el.content}
+          </div>
+        ))
+      )}
+    </>
+  );
 }
