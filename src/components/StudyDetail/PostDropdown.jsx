6ac05225d3d577ebf4060cee9a07648779c7ce52
JKyEun <j56237@naver.com>
2023-04-18 17:53:50 +0900

    Fix: 서로 팔로우하는 사이가 아닐 경우 1:1채팅이 뜨지 않도록 변경

--- src/components/StudyDetail/PostDropdown.jsx
+++ src/components/StudyDetail/PostDropdown.jsx
@@ -1,15 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../../style/postDropdown.scss';
+import { useDispatch, useSelector } from 'react-redux';
+import { convertOpen, setOtherSide } from '../../store/modules/chat';
 
 export default function PostDropdown({ writerInfo }) {
+  const dispatch = useDispatch();
+  const userInfo = useSelector((state) => state.user);
+  const isFollowing = userInfo.following.filter(
+    (el) => el.userId === writerInfo.userId,
+  );
+  const isFollowed = userInfo.follower.filter(
+    (el) => el.userId === writerInfo.userId,
+  );
+  let isFriend = false;
+
+  if (isFollowing.length > 0 && isFollowed.length > 0) {
+    isFriend = true;
+  }
+
+  const setChatting = () => {
+    dispatch(convertOpen());
+    dispatch(setOtherSide(writerInfo));
+  };
+
   return (
     <>
       <div className="postDropdown">
         <ul>
-          <li>
-            <Link to={`/mycity`}>1:1 채팅</Link>
-          </li>
+          {isFriend && (
+            <li>
+              <span onClick={setChatting}>1:1 채팅</span>
+            </li>
+          )}
           <li>
             <Link to={`/mycity/${writerInfo.userId}`}>정보 보기</Link>
           </li>
